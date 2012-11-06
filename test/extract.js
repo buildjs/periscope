dd9a64496825c6007eafbd4f05a11aab9735362d
Damon Oehlman <damon.oehlman@sidelab.com>
2012-11-06 17:02:28 +1000

    Minor restructure

--- test/extract.js
+++ test/extract.js
@@ -1,33 +1,32 @@
 var path = require('path'),
     fs = require('fs'),
     samplePath = path.resolve(__dirname, 'samples'),
+    files = fs.readdirSync(samplePath),
     esprima = require('esprima'),
     periscope = require('../');
     
-fs.readdir(samplePath, function(err, files) {
-    describe('sample context marking', function() {
-        (files || []).forEach(function(file) {
-            it('should be able to determine scope within: ' + file, function() {
-                var data = fs.readFileSync(path.join(samplePath, file), 'utf8'),
-                    ast = esprima.parse(data);
+describe('sample context marking', function() {
+    (files || []).forEach(function(file) {
+        it('should be able to determine scope within: ' + file, function() {
+            var data = fs.readFileSync(path.join(samplePath, file), 'utf8'),
+                ast = esprima.parse(data);
 
-                // write the scope data to a reference file
-                fs.writeFileSync(
-                    path.resolve(__dirname, 'meta', path.basename(file, '.js') + '.prescope.json'), 
-                    JSON.stringify(ast, null, 2), 
-                    'utf8'
-                );
+            // write the scope data to a reference file
+            fs.writeFileSync(
+                path.resolve(__dirname, 'meta', path.basename(file, '.js') + '.prescope.json'), 
+                JSON.stringify(ast, null, 2), 
+                'utf8'
+            );
 
-                // add the scope data to the ast
-                periscope(ast);
-                
-                // write the scope data to a reference file
-                fs.writeFileSync(
-                    path.resolve(__dirname, 'meta', path.basename(file, '.js') + '.json'), 
-                    JSON.stringify(ast, null, 2), 
-                    'utf8'
-                );
-            });
+            // add the scope data to the ast
+            periscope(ast);
+            
+            // write the scope data to a reference file
+            fs.writeFileSync(
+                path.resolve(__dirname, 'meta', path.basename(file, '.js') + '.json'), 
+                JSON.stringify(ast, null, 2), 
+                'utf8'
+            );
         });
     });
 });
